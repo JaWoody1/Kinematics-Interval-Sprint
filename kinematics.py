dbf7e840507deb80afdc315d58d968ae82e379d6
Patrick King <pking23@gmu.edu>
2024-01-24 23:00:22 -0500

    commit

--- kinematics.py
+++ kinematics.py
@@ -1,1 +1,22 @@
 ##Take in user data for a user defined amount of interval to calculate: ∆Position, ∆Time, Velocity, Time of Velocity, ∆Velocity, Acceleration, ∆Time of Acceleration
+
+##How many intervals will the user use
+
+intervals = input("Please input how many intervals were used: ")
+intervalLength = int(input("Please input how long each interval was (meters): "))
+
+##lists
+
+position = [0]
+time = [0]
+changeInPosition = [0]
+
+for x in range(int(intervals)):
+
+    #append values the user inputs
+
+    position.append(intervalLength + position[x])
+    time.append(int(input(f"Time at interval {x+1}: ")))
+
+
+    
---
+++ methods.py
@@ -0,0 +1,5 @@
+#methods
+
+def changeInTime (time1, time2):
+    t = time1 - time2
+    return t
