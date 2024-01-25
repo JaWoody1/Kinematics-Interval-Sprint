741843d86fdcb0301125a189ed79e693a778c223
Patrick King <pking23@gmu.edu>
2024-01-24 23:42:14 -0500

    commit

--- kinematics.py
+++ kinematics.py
@@ -1,22 +1,64 @@
 ##Take in user data for a user defined amount of interval to calculate: ∆Position, ∆Time, Velocity, Time of Velocity, ∆Velocity, Acceleration, ∆Time of Acceleration
 
+import methods
+
+
 ##How many intervals will the user use
 
 intervals = input("Please input how many intervals were used: ")
-intervalLength = int(input("Please input how long each interval was (meters): "))
+intervalLength = float(input("Please input how long each interval was (meters): "))
 
 ##lists
 
 position = [0]
 time = [0]
 changeInPosition = [0]
+changeInTime = [0]
+velocity = [0]
+timeOfVelocity = [0]
+changeInVelocity = [0]
+acceleration = [0]
+
 
 for x in range(int(intervals)):
 
     #append values the user inputs
 
     position.append(intervalLength + position[x])
-    time.append(int(input(f"Time at interval {x+1}: ")))
+    changeInPosition.append(intervalLength)
+    time.append(float(input(f"Time at interval {x+1}: ")))
+
+for y in range(int(len(position))):
+
+    #first value is already 0
+    if y>0:
+        changeInTime.append(methods.changeInTime(time[y], time[y-1]))
+        velocity.append(methods.velocity(changeInPosition[y], changeInTime[y]))
+        timeOfVelocity.append(methods.timeOfVelcoity(time[y], time[y-1]))
+        changeInVelocity.append(methods.changeInVelocity(velocity[y], velocity[y-1]))
+        acceleration.append(methods.acceleration(changeInVelocity[y], changeInTime[y]))
+
+for z in range(int(len(position))):
+
+    changeInTime[z] = methods.cleanUp(changeInTime[z])
+    velocity[z] = methods.cleanUp(velocity[z])
+    timeOfVelocity[z] = methods.cleanUp(timeOfVelocity[z])
+    changeInVelocity[z] = methods.cleanUp(changeInVelocity[z])
+    acceleration[z] = methods.cleanUp(acceleration[z])
+
+
+
+
+print(f"Change in Position: {changeInPosition}")
+print(f"Change in Time: {changeInTime}")
+print(f"Acceleration: {acceleration}")
+    
+
+
+
+
+
+    
 
 
     
--- methods.py
+++ methods.py
@@ -3,3 +3,24 @@
 def changeInTime (time1, time2):
     t = time1 - time2
     return t
+
+def velocity (meters, time):
+    v = meters/time
+    return v
+
+def timeOfVelcoity (time1, time2):
+    t = (time1 + time2)/2
+    return t
+
+def changeInVelocity (velocity1, velocity2):
+    cV = velocity1 - velocity2
+    return cV
+
+def acceleration (changeInVelocity, changeInTime):
+    a = changeInVelocity/changeInTime
+    return a
+
+def cleanUp (i):
+    i = float(i)
+    i = round(i, 2)
+    return i
